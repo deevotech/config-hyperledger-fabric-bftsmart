00debe92996da1da1f88c25402532be13f1a7712
datlv <datle2011@gmail.com>
2018-07-28 10:30:56 +0700

    fix generate config orderer

--- scripts/generate-orderer-tls.sh
+++ scripts/generate-orderer-tls.sh
@@ -31,7 +31,7 @@ source $(dirname "$0")/env.sh
 ORG=${g}
 mkdir -p ${DATA}
 initOrdererVars $ORG ${n}
-export ORDERER_GENERAL_LOCALMSPDIR=${DATA}/orgs/msp
+export ORDERER_GENERAL_LOCALMSPDIR=${DATA}/orgs/${ORG}
 export ORDERER_GENERAL_GENESISFILE=${DATA}/genesis.block
 export ORDERER_GENERAL_LOCALMSPID=${ORG}MSP
 export ORDERER_GENERAL_TLS_ROOTCAS=[${DATA}/${ORG}-ca-cert.pem]
