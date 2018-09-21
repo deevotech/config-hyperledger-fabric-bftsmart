9f9fd3fcb4c7e25771d35e156b294eb640257350
Quang Phan <pquocquang1102@gmail.com>
2018-09-21 16:09:26 +0700

    update setup environment scripts

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -4,11 +4,11 @@
 # Install go
 sudo apt-get update &&
 sudo apt-get -y upgrade &&
-sudo curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
-sudo tar -xf go1.9.1.linux-amd64.tar.gz
+sudo curl -O https://storage.googleapis.com/golang/go1.10.4.linux-amd64.tar.gz
+sudo tar -xf go1.10.4.linux-amd64.tar.gz
 sudo mv go /opt
-sudo mkdir -p /opt/gopath
-sudo chmod 777 -R /opt/gopath
+# sudo mkdir -p /opt/gopath
+# sudo chmod 777 -R /opt/gopath
 
 # Install JAVA
 sudo apt -y install openjdk-8-jdk &&
@@ -34,23 +34,21 @@ sudo make install
 cd $GOPATH/src/github.com 
 mkdir hyperledger
 cd hyperledger
-git clone https://github.com/datlv/hyperledger-fabric-bftsmart.git -b release-1.1
-mv hyperledger-fabric-bftsmart fabric
+git clone https://github.com/deevotech/fabric -b release-1.2-deevo
 cd fabric
 sudo ./devenv/setupUbuntuOnPPC64le.sh
 make dist-clean peer orderer configtxgen
 # Compile Hyperledger BFTSmart 
 cd $GOPATH/src/github.com/hyperledger
-git clone https://github.com/datlv/hyperledger-bftsmart-orderering.git -b release-1.1
-mv hyperledger-bftsmart-orderering hyperledger-bftsmart
+git clone https://github.com/deevotech/fabric-orderingservice -b release-1.2-deevo
+mv fabric-orderingservice hyperledger-bftsmart
 cd hyperledger-bftsmart
 ant
 
 # Install Fabric CA
 cd $GOPATH/src/github.com/hyperledger
 sudo apt install libtool libltdl-dev make
-git clone https://github.com/hyperledger/fabric-ca.git -b release-1.1
-sed -i 's/var Version string/var Version = "1.1.0"/' fabric-ca/lib/metadata/version.go
+git clone https://github.com/deevotech/fabric-ca -b release-1.2-deevo
 cd fabric-ca/cmd/fabric-ca-client
 go build
 cd ../fabric-ca-server
