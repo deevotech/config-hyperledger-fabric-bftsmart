ed1ec29c379980744d176ce0e86951bbea0caa78
Quang Phan <pquocquang1102@gmail.com>
2018-09-24 09:50:34 +0700

    install docker compose

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -9,3 +9,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update &&
 sudo apt-get -y install docker-ce &&
 sudo usermod -a -G docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
